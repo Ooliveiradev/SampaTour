caf95422f437b4eb11c320aff3f0a6ef745b9cd8
Danilo Ribeiro <danilorib2324@gmail.com>
2025-05-13 20:18:18 -0300

    Update map.js

--- assets/js/page/map.js
+++ assets/js/page/map.js
@@ -52,20 +52,20 @@ var iconeTemplo = L.icon({
   
   L.marker([-23.53744, -46.60653], { icon: iconeTemplo })
     .addTo(map)
-    .bindPopup('<div class="popup-content"><strong class="popup-title">Templo de Salomão</strong><br><img src="/assets/images/page/imagens/salomao.jpeg" height="200px" class="popup-image"><br><p class="popup-p">Um imponente templo religioso da Igreja Universal, localizado no Brás. Inspirado no templo bíblico, é um marco arquitetônico de São Paulo.</p></div>');
+    .bindPopup('<div class="popup-content"><strong class="popup-title">Templo de Salomão</strong><br><img src="assets/images/page/imagens/salomao.jpeg" height="200px" class="popup-image"><br><p class="popup-p">Um imponente templo religioso da Igreja Universal, localizado no Brás. Inspirado no templo bíblico, é um marco arquitetônico de São Paulo.</p></div>');
 
   
   L.marker([-23.56118, -46.65657], { icon: iconePaulista })
     .addTo(map)
-    .bindPopup('<div class="popup-content"><strong class="popup-title">Avenida Paulista</strong><br><img src="/assets/images/page/imagens/paulista.jpeg" height="200px" class="popup-image"><br><p class="popup-p">Uma das avenidas mais icônicas de São Paulo, centro financeiro, cultural e turístico com museus, shoppings e eventos.</p></div>');
+    .bindPopup('<div class="popup-content"><strong class="popup-title">Avenida Paulista</strong><br><img src="assets/images/page/imagens/paulista.jpeg" height="200px" class="popup-image"><br><p class="popup-p">Uma das avenidas mais icônicas de São Paulo, centro financeiro, cultural e turístico com museus, shoppings e eventos.</p></div>');
   
   L.marker([-23.56120, -46.65588], { icon: iconeMuseu })
     .addTo(map)
-    .bindPopup('<div class="popup-content"><strong class="popup-title">MASP</strong><br><img src="/assets/images/page/imagens/museu.jpeg" height="200px" class="popup-image"><br><p class="popup-p">O Museu de Arte de São Paulo é famoso por seu design suspenso e por abrigar uma das coleções mais importantes da América Latina.</p></div>');
+    .bindPopup('<div class="popup-content"><strong class="popup-title">MASP</strong><br><img src="assets/images/page/imagens/museu.jpeg" height="200px" class="popup-image"><br><p class="popup-p">O Museu de Arte de São Paulo é famoso por seu design suspenso e por abrigar uma das coleções mais importantes da América Latina.</p></div>');
 
     L.marker([-23.537493, -46.627012], { icon: iconeMuseu2 })
     .addTo(map)
-    .bindPopup('<div class="popup-content"><strong class="popup-title">Museu Catavento</strong><br><img src="/assets/images/page/imagens/museu2.jpeg" height="200px" class="popup-image"><br><p class="popup-p">Um museu interativo de ciência e tecnologia voltado para jovens e crianças, com exposições dinâmicas sobre física, química, biologia e astronomia.</p></div>');
+    .bindPopup('<div class="popup-content"><strong class="popup-title">Museu Catavento</strong><br><img src="assets/images/page/imagens/museu2.jpeg" height="200px" class="popup-image"><br><p class="popup-p">Um museu interativo de ciência e tecnologia voltado para jovens e crianças, com exposições dinâmicas sobre física, química, biologia e astronomia.</p></div>');
 
     // Cria um controle personalizado
 // Cria um controle personalizado
